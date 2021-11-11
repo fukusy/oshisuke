63720d36923fb7d4ed20cb6e6c98549843f18ce0
fukusy <fuku1study@gmail.com>
2021-11-11 14:36:00 +0000

    [Add]マイページ、プロフィール編集追加

--- app/controllers/user/events_controller.rb
+++ app/controllers/user/events_controller.rb
@@ -1,30 +1,35 @@
 class User::EventsController < ApplicationController
 
   def index
+    # @event = Event.new
+    # @events = Event.all
   end
-  
+
   def create
     @event = Event.new(event_parameter)
+    # @event.save!
+    # redirect_to events_path
   end
-  
+
   def show
   end
-  
+
   def destroy
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
-  
-  
+
+
+
   private
-  
+
   def event_parameter
-    
+    # params.require(:event).permit(:event_date, :event_time, :event_name, :event_content, :tag_id)
+
   end
 
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '4df3a0a6501ac61124a5c108112bf948c1ef744ac3a5c112cc06540d5a25c25e324832fd12896a14a6f6a9ea648ba9289608265f311356b6b9e3d69fede77e96'
