975438cc45bb0f21fc109316e448c29246850b7f
fukusy <fuku1study@gmail.com>
2021-11-27 13:24:39 +0000

    [Fix]マイページのuser_events修正

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -3,12 +3,15 @@ class User::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 
-    # @user_events = @user.events
-    # @user_relationship_events = @user.joined_events
-    relation_array = RelationshipEvent.where(user_id: @user.id).pluck(:event_id)
-    user_part_array = Event.where(user_id: @user.id).pluck(:id)
-    relation_array.push(user_part_array)
-    @user_events = Event.find(relation_array)
+      # @user_events = @user.events
+      # @user_relationship_events = @user.joined_events
+    # if relation_array.present?
+      relation_array = RelationshipEvent.where(user_id: @user.id).pluck(:event_id)
+      user_part_array = Event.where(user_id: @user.id).pluck(:id)
+      relation_array.push(user_part_array)
+      @user_events = Event.find(relation_array.flatten!)
+
+    # end
 
     # @user_relationship_events = RelationshipEvent.find(params[:user_id]).where("event_id", event_id)
   end
