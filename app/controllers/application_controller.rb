a53a95a16cf88de18813cf1a658e11f145d29055
fukusy <fuku1study@gmail.com>
2021-11-27 04:34:21 +0000

    [Add]メール設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_admin!, if: :admin_url 
+  # before_action :authenticate_admin!, if: :admin_url
   before_action :authenticate_user!,except: [:top, :attention]
 
   # パスに「/admin」が含まれているかどうかを確認する。
-  def admin_url
-    request.fullpath.include?("/admin")
-  end
+  # def admin_url
+  #   request.fullpath.include?("/admin")
+  # end
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :use_rules])
   end
-  
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,19 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'smtp.gmail.com',
+    user_name:            ENV['YOUR_EMAIL_ADDRESS'],
+    password:             ENV['YOUR_MAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
+
 end
